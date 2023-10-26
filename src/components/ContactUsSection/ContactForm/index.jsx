f89c2194c133289426ed062a83030c00c5aebf1c
Mykola Rudenko <98877409+MykolaRudenko7@users.noreply.github.com>
2023-10-26 16:38:47 +0300

    form layout

--- src/components/ContactUsSection/ContactForm/index.jsx
+++ src/components/ContactUsSection/ContactForm/index.jsx
@@ -1,83 +1,151 @@
 'use client'
 
-import { useRef, useState } from 'react'
+import { useId } from 'react'
+import cn from 'classnames'
 import styles from 'components/ContactUsSection/ContactForm/ContactForm.module.scss'
+import CreatableSelect from 'react-select/creatable'
+import { contactCountryOptions } from 'data/contactUsSectionData'
 
 export default function ContactForm() {
-  const formRef = useRef(null)
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phoneNumber: '',
-    message: '',
-  })
+  const contactFormId = useId()
+  const firstNameInputId = useId()
+  const lastNameInputId = useId()
+  const emailInputId = useId()
+  const phoneNumberInputId = useId()
+  const marketToContactSelectId = useId()
+  const yourMessageTextareaId = useId()
 
-  const handleFormInput = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value })
-  }
-  const handleSubmitFormData = (e) => {
-    e.preventDefault()
-    formRef.current.reset()
+  const customStyles = {
+    control: (baseStyles, state) => ({
+      ...baseStyles,
+      fontSize: '14px',
+      padding: '5px',
+      borderRadius: '10px',
+      backgroundColor: '#d9d9d956',
+      backdropFilter: 'blur(45px)',
+      border: '2px solid transparent',
+      boxShadow: 'none',
+      '&:hover': {
+        border: '2px solid white',
+      },
+    }),
+    option: (baseStyles) => ({
+      ...baseStyles,
+      backgroundColor: '#3a3d60',
+      fontSize: '14px',
+    }),
+    menu: (baseStyles) => ({
+      ...baseStyles,
+      zIndex: '5',
+    }),
+    menuList: (baseStyles) => ({
+      ...baseStyles,
+      padding: 0,
+    }),
+    placeholder: (baseStyles) => ({
+      ...baseStyles,
+      color: '#fff',
+      opacity: '0.5',
+    }),
+    input: (baseStyles) => ({
+      ...baseStyles,
+      color: '#fff',
+    }),
+    singleValue: (baseStyles) => ({ ...baseStyles, color: '#fff' }),
   }
 
   return (
-    <form className={styles.formBlock} onSubmit={handleSubmitFormData} ref={formRef}>
-      <label className={styles.formBlockLabel} htmlFor="name">
+    <form className={styles.formBlock} id={contactFormId}>
+      <div className={styles.inputContainer}>
         <input
-          autoComplete="name"
-          className={styles.formBlockInput}
-          id="name"
-          name="name"
-          onChange={handleFormInput}
-          placeholder="Name"
+          autoComplete="on"
+          className={styles.input}
+          id={firstNameInputId}
+          name="firstName"
+          tabIndex="0"
           type="text"
           required
         />
-      </label>
-      <label className={styles.formBlockLabel} htmlFor="email">
+        <label className={styles.label} htmlFor={firstNameInputId}>
+          Name
+        </label>
+      </div>
+
+      <div className={styles.inputContainer}>
         <input
-          autoComplete="email"
-          className={styles.formBlockInput}
-          id="email"
+          autoComplete="on"
+          className={styles.input}
+          id={lastNameInputId}
+          name="lastName"
+          tabIndex="0"
+          type="text"
+          required
+        />
+        <label className={styles.label} htmlFor={lastNameInputId}>
+          Last Name
+        </label>
+      </div>
+
+      <div className={styles.inputContainer}>
+        <input
+          autoComplete="on"
+          className={styles.input}
           name="email"
-          onChange={handleFormInput}
-          placeholder="Email"
+          tabIndex="0"
           type="email"
           required
         />
-      </label>
-      <label className={styles.formBlockLabel} htmlFor="phoneNumber">
+        <label className={styles.label} htmlFor={emailInputId}>
+          E-mail
+        </label>
+      </div>
+
+      <div className={styles.inputContainer}>
         <input
-          autoComplete="phone number"
-          className={styles.formBlockInput}
-          id="phoneNumber"
-          name="phoneNumber"
-          onChange={handleFormInput}
-          placeholder="Phone Number"
+          autoComplete="on"
+          className={styles.input}
+          id={phoneNumberInputId}
+          name="phone"
+          tabIndex="0"
           type="tel"
           required
         />
-      </label>
-      <label className={styles.formBlockLabel} htmlFor="message">
-        <textarea
-          autoComplete="message"
-          className={styles.formBlockTextarea}
-          id="message"
-          maxLength="150"
-          name="message"
-          onChange={handleFormInput}
-          placeholder="Your Message"
-          rows="5"
+        <label className={styles.label} htmlFor={phoneNumberInputId}>
+          Phone Number
+        </label>
+      </div>
+
+      <div className={cn(styles.inputContainerSelect, styles.inputContainer)}>
+        <CreatableSelect
+          id={marketToContactSelectId}
+          isCreatable={true}
+          isSearchable={true}
+          name="country"
+          options={contactCountryOptions}
+          placeholder={'Market to contact'}
+          styles={customStyles}
+          tabIndex="0"
+          required
         />
-      </label>
-      <button
-        about="submit form"
-        className={styles.submitButton}
-        tabIndex="0"
-        type="submit"
-        value="Submit"
-      >
-        Schedule a Call
+      </div>
+
+      <div className={styles.inputContainer}>
+        <textarea
+          autoComplete="on"
+          className={cn(styles.input, styles.textarea)}
+          id={yourMessageTextareaId}
+          name="message"
+          rows="5"
+          tabIndex="0"
+          required
+        ></textarea>
+        <label className={styles.label} htmlFor={yourMessageTextareaId}>
+          Your message
+        </label>
+      </div>
+
+      <button aria-label="submit" className={styles.submitButton} tabIndex="0" type="submit">
+        Submit Your Idea
       </button>
     </form>
   )
--- src/data/contactUsSectionData.js
+++ src/data/contactUsSectionData.js
@@ -5,3 +5,17 @@ export const contactUsSectionData = {
   text: 'Connect with TECH COSSACKS and embark on a transformative journey.',
   imageSrs: contactUsImage,
 }
+
+export const contactCountryOptions = [
+  { value: 'Ukraine', label: 'Ukraine' },
+  { value: 'United Kingdom', label: 'United Kingdom' },
+  { value: 'United States', label: 'United States' },
+  { value: 'Canada', label: 'Canada' },
+  { value: 'Australia', label: 'Australia' },
+  { value: 'Germany', label: 'Germany' },
+  { value: 'France', label: 'France' },
+  { value: 'Spain', label: 'Spain' },
+  { value: 'Italy', label: 'Italy' },
+  { value: 'Japan', label: 'Japan' },
+  { value: '', label: 'Write other here...' },
+]
