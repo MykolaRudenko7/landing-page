5c8f90bcd328a4aaf28a0f875f2ca83acddee87d
Mykola Rudenko <98877409+MykolaRudenko7@users.noreply.github.com>
2023-10-27 16:00:29 +0300

    footer image

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,14 +2,13 @@ import { Poppins } from 'next/font/google'
 import 'styles/global.scss'
 
 export const metadata = {
-  title: 'Home page',
+  title: 'Tech Cossacks',
   description: 'Your Trusted Partner for Web Development Excellence',
 }
 
 const poppins = Poppins({
   weight: ['300', '400', '500', '600', '700'],
   subsets: ['latin'],
-  variable: '--mainFont',
   display: 'swap',
 })
 
